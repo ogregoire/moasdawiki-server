cc03ec7731839a5b908b2bd5b185a3ba34e1e10b
Herbert Reiter <4941275-moasda@users.noreply.gitlab.com>
2021-01-04 11:39:39 +0100

    Update copyright year

--- src/main/java/net/moasdawiki/service/search/SearchResult.java
+++ src/main/java/net/moasdawiki/service/search/SearchResult.java
@@ -1,6 +1,6 @@
 /*
  * MoasdaWiki Server
- * Copyright (C) 2008 - 2020 Herbert Reiter (herbert@moasdawiki.net)
+ * Copyright (C) 2008 - 2021 Herbert Reiter (herbert@moasdawiki.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
